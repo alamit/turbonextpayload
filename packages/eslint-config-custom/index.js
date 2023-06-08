bb9ba2e3a4499d4afbc7da8d9f8fa7dbfc8c1ecd
alamit <adrien@lamit.com>
2023-06-08 02:26:52 +0200

    setup monorepo & IDE settings

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier", "plugin:json/recommended"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
   },
